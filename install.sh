806be336454e1036a56bd98722f11ad2c8c87244
David Wright <dave@ritey.com>
2022-07-06 16:42:11 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+{
+    
+while getopts k: flag
+do
+    case "${flag}" in
+        k) APIKEY=${OPTARG};;
+    esac
+done
+
+if [ -z "$APIKEY" ]; then
+    echo "No API key provided, your API key can be found in your https://wafhub.com account."
+    exit 0;
+fi
+
+cat <<EOF > /srv/www/wafhub-remote/run.sh
+#!/bin/bash
+
+allLogFiles () {
+    LOGFILES=/var/log/nginx/*
+
+    for f in \$LOGFILES
+    do
+        filesize=\$(stat -c%s "\$f")
+        if [ \$filesize ]; then
+            curl https://wafhub.com/api/logs -H "Authorization: Bearer $APIKEY" -F log=@\$f
+        fi
+        sleep 10s
+    done
+
+    LOGFILES=/var/log/apache2/*
+    for f in \$LOGFILES
+    do
+        filesize=\$(stat -c%s "\$f")
+        if [ \$filesize ]; then
+            curl https://wafhub.com/api/logs -H "Authorization: Bearer $APIKEY" -F log=@\$f
+        fi
+
+        sleep 10s
+    done
+    unset \$LOGFILES
+}
+
+allLogFiles
+
+EOF
+
+cat <<EOF > /srv/www/wafhub-remote/wafhub.service
+[Unit]
+Description=WAFHUB Log Service
+
+[Service]
+User=root
+Type=simple
+TimeoutSec=0
+PIDFile=/run/wafhub.pid
+ExecStart=/bin/bash /srv/www/wafhub-remote/run.sh > /dev/null 2>/dev/null
+ExecStop=/bin/kill -HUP \$MAINPID
+ExecReload=/bin/kill -HUP \$MAINPID
+KillMode=process
+
+Restart=on-failure
+RestartSec=42s
+
+StandardOutput=null
+StandardError=null
+[Install]
+WantedBy=default.target
+EOF
+
+sudo rm /etc/systemd/system/wafhub.service
+sudo ln -s /srv/www/wafhub-remote/wafhub.service /etc/systemd/system/wafhub.service
+
+sudo systemctl daemon-reload
+sudo systemctl start wafhub
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# WAFHUB Remote Server
+
+Send your Apache or Nginx logs automatically to WAFHUB. You need to get an API key first from https://wafhub.com.
+
+Simply run at the terminal: wget -P /srv/www/wafhub-remote https://raw.githubusercontent.com/coderstudios/wafhub-remote/main/install.sh && sudo bash /srv/www/wafhub-remote/install.sh -k YOUR-API-KEY
