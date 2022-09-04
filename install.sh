60843f908f8d45ce3ef5de45c3491c6c456fe966
David Wright <dave@ritey.com>
2022-09-04 12:34:41 +0100

    Move API to a subdomain to prevent impact on main site

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ allLogFiles () {
     do
         filesize=\$(stat -c%s "\$f")
         if [ \$filesize ]; then
-            curl https://wafhub.com/api/logs -H "Authorization: Bearer $APIKEY" -F "web=nginx" -F "host=$HOSTNAME" -F "log=@\$f"
+            curl https://api.wafhub.com/logs -H "Authorization: Bearer $APIKEY" -F "web=nginx" -F "host=$HOSTNAME" -F "log=@\$f"
         fi
         sleep 10s
     done
@@ -34,7 +34,7 @@ allLogFiles () {
     do
         filesize=\$(stat -c%s "\$f")
         if [ \$filesize ]; then
-            curl https://wafhub.com/api/logs -H "Authorization: Bearer $APIKEY" -F "web=apache" -F "host=$HOSTNAME" -F "log=@\$f"
+            curl https://api.wafhub.com/logs -H "Authorization: Bearer $APIKEY" -F "web=apache" -F "host=$HOSTNAME" -F "log=@\$f"
         fi
 
         sleep 10s
